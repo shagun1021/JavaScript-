6afa641e954c3ab24c198eeebe79bbaf3c2823d2
Shagun <154508782+shagun1021@users.noreply.github.com>
2024-04-18 00:44:56 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,15 @@
+
+const arr=[1,2,3,4,5,6,7,8,9];
+
+
+// let result=arr.map((item)=>
+// {
+//      return item+=10;
+// })
+
+
+// ::::::short hand:::::::::::::::
+
+let result=arr.map((item)=>item+=10);
+
+console.log(result);
